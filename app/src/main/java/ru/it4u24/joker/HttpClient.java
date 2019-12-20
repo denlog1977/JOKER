e4cb8b4877ab0bebaa81ea77ef7039e0280f4443
Dmitriy33g <Dmitriy33g@gmail.com>
2019-12-20 15:49:35 +0300

    AsyncTask

---
+++ app/src/main/java/ru/it4u24/joker/HttpClient.java
@@ -0,0 +1,11 @@
+package ru.it4u24.joker;
+
+import android.os.AsyncTask;
+
+public class HttpClient extends AsyncTask<String, Integer, String[][]> {
+
+    @Override
+    protected String[][] doInBackground(String... strings) {
+        return new String[0][];
+    }
+}
