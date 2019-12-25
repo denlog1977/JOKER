0924a8295d393b59ed996188a73c2b16d40daa5a
den <denlog1977@gmail.com>
2019-12-25 19:24:51 +0300

    joker.jpg

--- app/src/main/java/ru/it4u24/joker/Keys.java
+++ app/src/main/java/ru/it4u24/joker/Keys.java
@@ -1,6 +1,27 @@
 package ru.it4u24.joker;
 
+import androidx.annotation.NonNull;
+
 public enum Keys {
-    service1cLog,
-    service1cPas;
+
+    service1cLog ("Логин  для Web-Service 1c"),
+    service1cPas ("Пароль для Web-Service 1c");
+
+    private String title;
+
+    Keys(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    @NonNull
+    @Override
+    public String toString() {
+        return title;
+    }
+
 }
--- app/src/main/java/ru/it4u24/joker/MainActivity.java
+++ app/src/main/java/ru/it4u24/joker/MainActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+//        TextView textView = findViewById(R.id.textView);
+//        textView.setText(Keys.service1cLog.toString() + " " + Keys.service1cPas.toString());
 
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
