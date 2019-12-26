a741556f9d929c5488aea93516add2348fb8f40d
denlog1977 <denlog1977@gmail.com>
2019-12-26 17:01:59 +0300

    den - chooseDate

--- app/src/main/java/ru/it4u24/joker/QueueActivity.java
+++ app/src/main/java/ru/it4u24/joker/QueueActivity.java
@@ -1,5 +1,6 @@
 package ru.it4u24.joker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,15 +8,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
 public class QueueActivity extends AppCompatActivity {
 
     private Spinner spinner;
     private ArrayList<Rc> rcArrayList;
+    private CalendarView mDateCalendar;
+    private long mDate;
+    private Button mChooseDate;
+    private String mDateTxt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +33,43 @@ public class QueueActivity extends AppCompatActivity {
 
         initViews();
 
+
     }
 
     private void initViews() {
         spinner = findViewById(R.id.spinner);
         initSpinner();
+        mChooseDate = findViewById(R.id.chooseDate);
+        mDateCalendar = findViewById(R.id.dateCalendar);
+        mDateCalendar.setVisibility(View.GONE);
+
+        mChooseDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDateCalendar.setVisibility(View.VISIBLE);
+            }
+        });
+
+        mDateCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+
+            }
+        });
+
+        mDateCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                mDateTxt = i+"-"+i1+"-"+i2;
+                mChooseDate.setText("Дата: " + mDateTxt);
+                GregorianCalendar gregorianCalendar = new GregorianCalendar();
+                gregorianCalendar.set(i, i1, i2);
+                mDate = gregorianCalendar.getTimeInMillis();
+                calendarView.setVisibility(View.GONE);
+            }
+        });
+
+
     }
 
     private void initSpinner() {
