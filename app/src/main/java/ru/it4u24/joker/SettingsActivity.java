b2b2c815106464eb3a7d713e695788da6cf712d4
Dmitriy33g <Dmitriy33g@gmail.com>
2020-01-24 18:13:05 +0300

    dag - картинка настроек

--- app/src/main/java/ru/it4u24/joker/SettingsActivity.java
+++ app/src/main/java/ru/it4u24/joker/SettingsActivity.java
@@ -1,16 +1,113 @@
 package ru.it4u24.joker;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
 
+        imageView = findViewById(R.id.ivPhoto);
+        CardView cardView = findViewById(R.id.cvPhoto);
+        registerForContextMenu(cardView);
+        /*cardView.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+             }
+        });*/
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Выбран фото", Toast.LENGTH_SHORT).show();
+            }
+        });
+        cardView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(getApplicationContext(), "Выбрано меню", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            cardView.setOnContextClickListener(new View.OnContextClickListener() {
+                @Override
+                public boolean onContextClick(View v) {
 
+                    Toast.makeText(getApplicationContext(), "Выбрано меню c индексом " + v.getId(), Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            });
+        }*/
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        switch (v.getId()) {
+            case R.id.cvPhoto:
+                menu.add(0, 1, 0, "Выбрать фото");
+                menu.add(0, 2, 0, "Удалить фото");
+                break;
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case 1:
+                Toast.makeText(getApplicationContext(), "Выбрано меню Выбрать фото", Toast.LENGTH_SHORT).show();
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, 1);
+                break;
+            case 2:
+                Toast.makeText(getApplicationContext(), "Выбрано меню Удалить фото", Toast.LENGTH_SHORT).show();
+                imageView.setImageResource(R.drawable.joker);
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap bitmap = null;
+
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = data.getData();
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    imageView.setImageBitmap(bitmap);
+                }
+                break;
+        }
     }
 }
